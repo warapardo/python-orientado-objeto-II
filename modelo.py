5c95a6cf6d33a7cd7fbe7c8ec9a23c6b8cbd947e
wara <waarapardo@gmail.com>
2024-04-13 23:00:06 -0300

    Avançando na Orientação a Objetos em Python

--- modelo.py
+++ modelo.py
@@ -1,57 +1,51 @@
-class Filme:
+class Programa:
+    def __init__(self, nome, ano):
+        self._nome = nome.title()
+        self.ano = ano
+        self._likes = 0
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self._nome = novo_nome.title()
+
+    @property
+    def likes(self):
+        return self._likes
+
+    def dar_likes(self):
+        self._likes += 1
+
+    def __str__(self):
+        return f'Nome: {self._nome} | Ano: {self.ano} | Likes: {self._likes}'
+
+class Filme(Programa):
     def __init__(self, nome, ano, duracao):
-        self.__nome = nome
-        self.ano = ano
+        super().__init__(nome, ano)
         self.duracao = duracao
-        self.__likes = 0
 
-    @property
-    def nome(self):
-        return self.__nome
+    def __str__(self):
+        return f'Nome: {self._nome} | Ano: {self.ano} | Duração: {self.duracao}min | Likes: {self._likes}'
 
-    @nome.setter
-    def nome(self, novo_nome):
-        self.__nome = novo_nome.title()
-
-    @property
-    def likes(self):
-        return self.__likes
-
-    def dar_likes(self):
-        self.__likes += 1
-
-
-class Serie:
+class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
-        self.__nome = nome.title()
-        self.ano = ano
+        super().__init__(nome, ano)
         self.temporadas = temporadas
-        self.__likes = 0
 
-    @property
-    def nome(self):
-        return self.__nome
+    def __str__(self):
+        return f'Nome: {self._nome} | Ano: {self.ano} | Temporadas: {self.temporadas} | Likes: {self._likes}'
 
-    @nome.setter
-    def nome(self, novo_nome):
-        self.__nome = novo_nome.title()
-
-    @property
-    def likes(self):
-        return self.__likes
-
-    def dar_likes(self):
-        self.__likes += 1
 
 vingadores = Filme("vingadores - guerra infinita", 2018, 160)
 vingadores.dar_likes()
-
-print(f"Nome: {vingadores.nome} | Ano: {vingadores.ano} | Duração: {vingadores.duracao} | Likes: {vingadores.likes}")
-
-atlanta = Serie("atlanta", 2018, 2)
+atlanta = Serie("atlanta", 2006, 2)
 atlanta.dar_likes()
 atlanta.dar_likes()
 
-print(f"Nome: {atlanta.nome} | Ano: {atlanta.ano} | Temporadas: {atlanta.temporadas} | Likes: {atlanta.likes}")
+filmes_e_series = [vingadores, atlanta]
 
-
+for programa in filmes_e_series:
+    print(programa)
